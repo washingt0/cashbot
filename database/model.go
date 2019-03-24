e4c8803f655e8539bd80776693404e881f661c51
Washington Santos <washingtonstk@gmail.com>
2019-03-24 17:07:54 -0300

    add tags to entries

--- database/model.go
+++ database/model.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -16,6 +17,13 @@ type Entry struct {
 	Owner       string    `json:"user"`
 	CreatedAt   time.Time `json:"created_at"`
 	Description string    `json:"description"`
+	Tags        []string  `json:"tags"`
+}
+
+type Tag struct {
+	Name      string    `json:"string"`
+	Owner     string    `json:"user"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func (e *Entry) AddEntry(mgo *mongo.Client) error {
@@ -84,23 +92,17 @@ func (e *Entry) DropLastEntry(mgo *mongo.Client) error {
 	}
 	coll := mgo.Database("cashbot").Collection("entries")
 
-	doc := coll.FindOne(context.Background(), bson.D{
-		{"owner", e.Owner},
-	}, &options.FindOneOptions{Sort: bson.D{
-		{"createdat", -1},
-	}})
-
-	if bytes, err := doc.DecodeBytes(); err != nil {
+	_id, err := e.GetLastEntryId(mgo)
+	if err != nil {
 		return err
-	} else {
-		_id := bytes.Lookup("_id").ObjectID()
-
-		if _, err := coll.DeleteOne(context.Background(), bson.D{
-			{"_id", _id},
-		}); err != nil {
-			return err
-		}
 	}
+
+	if _, err := coll.DeleteOne(context.Background(), bson.D{
+		{"_id", _id},
+	}); err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -117,4 +119,92 @@ func (e *Entry) DropEntries(mgo *mongo.Client) error {
 		return err
 	}
 	return nil
+}
+
+func (e *Entry) GetLastEntryId(mgo *mongo.Client) (primitive.ObjectID, error) {
+	if e.Owner == "" {
+		return primitive.ObjectID{}, errors.New("Sorry, I didn't recognize you")
+	}
+	coll := mgo.Database("cashbot").Collection("entries")
+
+	doc := coll.FindOne(context.Background(), bson.D{
+		{"owner", e.Owner},
+	}, &options.FindOneOptions{Sort: bson.D{
+		{"createdat", -1},
+	}})
+
+	if bytes, err := doc.DecodeBytes(); err != nil {
+		return primitive.ObjectID{}, err
+	} else {
+		return bytes.Lookup("_id").ObjectID(), nil
+	}
+}
+
+func (e *Entry) AddTagToEntry(mgo *mongo.Client, tag string) error {
+	if e.Owner == "" {
+		return errors.New("Sorry, I didn't recognize you")
+	}
+	coll := mgo.Database("cashbot").Collection("entries")
+
+	_id, err := e.GetLastEntryId(mgo)
+	if err != nil {
+		return err
+	}
+
+	_, err = coll.UpdateOne(context.Background(), bson.D{
+		{"_id", _id},
+	}, bson.D{
+		{
+			"$addToSet", bson.D{
+				{"tags", tag},
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Tag) AddTag(mgo *mongo.Client) error {
+	coll := mgo.Database("cashbot").Collection("tags")
+	_, err := coll.InsertOne(context.Background(), t)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *Tag) GetAllTags(mgo *mongo.Client) ([]Tag, error) {
+	if t.Owner == "" {
+		return []Tag{}, errors.New("Sorry, I didn't recognize you")
+	}
+
+	filter := bson.D{
+		{"owner", t.Owner},
+	}
+
+	coll := mgo.Database("cashbot").Collection("tags")
+	cur, err := coll.Find(context.Background(), filter)
+	if err != nil {
+		return []Tag{}, err
+	}
+	defer cur.Close(context.Background())
+
+	var data []Tag
+	for cur.Next(context.Background()) {
+		var elem Tag
+		if err := cur.Decode(&elem); err != nil {
+			return []Tag{}, err
+		}
+
+		data = append(data, elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		return []Tag{}, err
+	}
+
+	return data, nil
 }
--- handler/message.go
+++ handler/message.go
@@ -17,26 +17,49 @@ import (
 	STATES
 	0 or nil - First contact
 	1 - Bot is waiting for the entry data
+	2 - Bot is waiting for the tag data
+	3 - Bot is waiting for the tag to add to a entry
+*/
+/*
+	REPLY
+	0 - None
+	1 - Markdown
+	2 - File
+	3 - Keyboard
 */
 
 func IncomingMessageHandler(m *tgbotapi.Message, mgo *mongo.Client) (tgbotapi.MessageConfig, bool, error) {
 	e := database.Entry{}
 
 	e.Owner = m.Chat.UserName
-	message, md, file, err := parseMessage(m.Text, mgo, &e)
+	message, reply, err := parseMessage(m.Text, mgo, &e)
 
+	file := false
 	msg := tgbotapi.NewMessage(m.Chat.ID, message)
-	if md {
+
+	if reply == 1 {
 		msg.ParseMode = tgbotapi.ModeMarkdown
+	} else if reply == 2 {
+		file = true
+	} else if reply == 3 {
+		var t database.Tag
+		t.Owner = e.Owner
+		if data, err := t.GetAllTags(mgo); err != nil {
+			return msg, false, err
+		} else {
+			msg.ReplyMarkup = makeReplyKeyboard(data)
+		}
+	} else if reply == -3 {
+		msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
 	}
 
 	return msg, file, err
 }
 
-func parseMessage(text string, mgo *mongo.Client, e *database.Entry) (string, bool, bool, error) {
+func parseMessage(text string, mgo *mongo.Client, e *database.Entry) (string, int, error) {
 
 	if text == "/start" {
-		return "Send the /addentry command to insert a new entry with this format `<VALUE> <DESCRIPTION>` or /help", false, false, nil
+		return "Send the /addentry command to insert a new entry with this format `<VALUE> <DESCRIPTION>` or /help", 0, nil
 	} else if text == "/help" {
 		return `
 /start - Start a conversation with the bot
@@ -46,76 +69,118 @@ func parseMessage(text string, mgo *mongo.Client, e *database.Entry) (string, bo
 /getmonthreport - Ask the bot to produce a report with the month information
 /getpdfreport - Ask the bot to produce a PDF report from your information
 /removelast - Ask the bot to remove the last entry
+/addtag - Ask the bot to receive a tag name
+/listtag - Ask the bot to list your tags
 /clear - Ask the bot to clear all your data
 /help - Display this help message
-`, false, false, nil
-	} else if text == "/addentry" {
-		if err := setState(e.Owner, 1); err != nil {
-			return "", false, false, err
-		}
-		return "Send the entry information, something like this: `14.00 pot`. If the value was less than zero, I will mark it as a payment", true, false, nil
+`, 0, nil
+		/* === BEGIN REPORTS === */
 	} else if text == "/getreport" {
 		if data, err := e.GetOwnerEntries(mgo, nil, nil); err != nil {
-			return "", false, false, err
+			return "", 0, err
 		} else {
-			return makeTable(data), true, false, nil
+			return makeTable(data), 1, nil
 		}
 	} else if text == "/getpdfreport" {
 		if data, err := e.GetOwnerEntries(mgo, nil, nil); err != nil {
-			return "", false, false, err
+			return "", 0, err
 		} else {
-			return report.GeneratePDF(data, e.Owner), false, true, nil
+			return report.GeneratePDF(data, e.Owner), 2, nil
 		}
 	} else if text == "/getdayreport" {
 		now := time.Now()
 		start := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 		end := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, now.Location())
 		if data, err := e.GetOwnerEntries(mgo, &start, &end); err != nil {
-			return "", false, false, err
+			return "", 0, err
 		} else {
-			return makeTable(data), true, false, nil
+			return makeTable(data), 1, nil
 		}
 	} else if text == "/getmonthreport" {
 		now := time.Now()
 		start := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
 		end := time.Date(now.Year(), now.Month()+1, 1, 0, 0, 0, 0, now.Location())
 		if data, err := e.GetOwnerEntries(mgo, &start, &end); err != nil {
-			return "", false, false, err
+			return "", 0, err
 		} else {
-			return makeTable(data), true, false, nil
+			return makeTable(data), 1, nil
 		}
+		/* === END REPORTS === */
+		/* === BEGIN ENTRIES === */
+	} else if text == "/addentry" {
+		if err := setState(e.Owner, 1); err != nil {
+			return "", 0, err
+		}
+		return "Send the entry information, something like this: `14.00 pot`. If the value was less than zero, I will mark it as a payment", 1, nil
 	} else if text == "/removelast" {
 		if err := e.DropLastEntry(mgo); err != nil {
-			return "", false, false, err
+			return "", 0, err
 		}
-		return "Okay, it's gone. ;)", false, false, nil
+		return "Okay, it's gone. ;)", 0, nil
 	} else if text == "/clear" {
 		if err := e.DropEntries(mgo); err != nil {
-			return "", false, false, err
+			return "", 0, err
 		}
-		return "Okay, everything is gone. ;)", false, false, nil
+		return "Okay, everything is gone. ;)", 0, nil
+		/* === END ENTRIES === */
+		/* === BEGIN TAGS === */
+	} else if text == "/addtag" {
+		if err := setState(e.Owner, 2); err != nil {
+			return "", 0, err
+		}
+		return "Send the tag name", 0, nil
+	} else if text == "/listtag" {
+		t := database.Tag{Owner: e.Owner}
+		if data, err := t.GetAllTags(mgo); err != nil {
+			return "", 0, err
+		} else {
+			return makeTagTable(data), 1, nil
+		}
+	} else if text == "/deletetag" {
+		/* === END TAGS === */
 	} else {
 		state, err := getState(e.Owner)
 		if err != nil {
-			return "", false, false, err
+			return "", 0, err
 		}
 		if state == 1 {
 			if err := parseEntryText(text, e); err != nil {
-				return "", false, false, err
+				return "", 0, err
 			} else {
 				if err := e.AddEntry(mgo); err != nil {
-					return "", false, false, err
+					return "", 0, err
 				}
-				if err := setState(e.Owner, 0); err != nil {
-					return "", false, false, err
+				if err := setState(e.Owner, 3); err != nil {
+					return "", 0, err
 				}
-				return "Ok, I will remember this.", false, false, nil
+				return "Ok, I will remember this.", 3, nil
 			}
 		} else if state == 2 {
+			var t database.Tag
+			t.Name = text
+			t.Owner = e.Owner
+			t.CreatedAt = time.Now()
+			if err := t.AddTag(mgo); err != nil {
+				return "", 0, err
+			}
+			setState(e.Owner, 0)
+			return "Ok, you can use this now.", 0, nil
+		} else if state == 3 {
+			if text == "Done" {
+				if err := setState(e.Owner, 0); err != nil {
+					return "", 0, err
+				}
+				return "Ok, done!", -3, nil
+			} else {
+				if err := e.AddTagToEntry(mgo, text); err != nil {
+					return "", 0, err
+				}
+				return "Ok, added!", 3, nil
+			}
 		}
 	}
 
-	return "Oops, I did not understand you :'( ", false, false, nil
+	return "Oops, I did not understand you :'( ", 0, nil
 }
 
 func parseEntryText(text string, e *database.Entry) error {
@@ -134,6 +199,7 @@ func parseEntryText(text string, e *database.Entry) error {
 			e.Description = strings.Join(parts[1:], " ")
 		}
 		e.CreatedAt = time.Now()
+		e.Tags = []string{}
 	}
 	return nil
 }
@@ -196,5 +262,47 @@ func makeTable(data []database.Entry) string {
 `
 	out += "Balance: " + strconv.FormatFloat(tin-tou, 'f', 2, 64) + `
 `
+	return out
+}
+
+func makeReplyKeyboard(data []database.Tag) tgbotapi.ReplyKeyboardMarkup {
+	var keys []tgbotapi.KeyboardButton
+	for _, t := range data {
+		keys = append(keys, tgbotapi.NewKeyboardButton(t.Name))
+	}
+
+	keys = append(keys, tgbotapi.NewKeyboardButton("Done"))
+
+	var keyboard tgbotapi.ReplyKeyboardMarkup
+
+	keyboard.ResizeKeyboard = true
+	keyboard.OneTimeKeyboard = true
+	keyboard.Selective = true
+
+	for i := 0; i < (len(keys)/3)+1; i++ {
+		row := []tgbotapi.KeyboardButton{}
+		for j := 0; j < 3; j++ {
+			if (i*3)+j >= len(keys) {
+				continue
+			} else {
+				row = append(row, keys[(i*3)+j])
+			}
+		}
+		keyboard.Keyboard = append(keyboard.Keyboard, row)
+	}
+
+	return keyboard
+}
+
+func makeTagTable(data []database.Tag) string {
+	out := `Your Tags
+`
+	for _, e := range data {
+		out += e.CreatedAt.Format("2006/01/02 15:04")
+		out += " | "
+		out += e.Name
+		out += `
+`
+	}
 	return out
 }
